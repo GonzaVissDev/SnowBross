679f5084c9f646e4c6412dfe392b442181551091
Gonzalo <57195786+GonzaVissDev@users.noreply.github.com>
2019-11-07 17:32:21 -0300

    adding player mechanics

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Player_Script player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GetComponent<Player_Script>();
+
+        GameObject PlayerObject = GameObject.FindWithTag("Player");
+        if (PlayerObject != null)
+        {
+            player = PlayerObject.GetComponent<Player_Script>();
+
+        }
+
+
+       rb.AddForce(new Vector2(6f* player.Player_Direction, 4f), ForceMode2D.Impulse);
+        if (player.Player_Direction == -1f)
+        {
+            rb.transform.Rotate(0f, -180f, 0);
+
+        }
+        Destroy(this.gameObject, 1f);
+    }
+
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Cuando toque con el enemigo.
+    }
+}
--- Assets/Script/Player_Script.cs
+++ Assets/Script/Player_Script.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Player_Script : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+    public GameObject Bullet;
+    public Transform Bullet_position;
     public float Player_Speed = 10f;
+    public float Player_Direction = 1f;
     public float Player_JumpForce = 10f;
     private bool Player_jump = false;
     public bool grounded = false;
+    public float Bullet_Cooldown = 0.5f;
+    public float Cooldown_Timer;
     private Animator Player_Animation;
     
     // Start is called before the first frame update
@@ -35,6 +40,22 @@ public class Player_Script : MonoBehaviour
             Player_Animation.SetTrigger("Shoot");
         }
 
+        if (Cooldown_Timer>0)
+        {
+            Cooldown_Timer -= Time.deltaTime;
+        }
+
+        if (Cooldown_Timer < 0)
+        {
+            Cooldown_Timer =0;
+        }
+        if (Input.GetButton("Fire1")&& Cooldown_Timer ==0)
+        {
+            Player_Animation.SetTrigger("Shoot");
+            Spawn_Bullet(true);
+            Cooldown_Timer = Bullet_Cooldown;
+        }
+
     }
 
     private void FixedUpdate()
@@ -49,10 +70,12 @@ public class Player_Script : MonoBehaviour
 
         if (PlayerSpeed_x> 0.1f)
         {
+            Player_Direction = 1f;
             transform.localScale = new Vector3(5f, 5f, 5f);
         }
         if (PlayerSpeed_x < -0.1f)
         {
+            Player_Direction = -1f;
             transform.localScale = new Vector3(-5f, 5f, 5f);
             
         }
@@ -66,5 +89,14 @@ public class Player_Script : MonoBehaviour
             Player_jump = false;
         }
         
+    }
+
+    void Spawn_Bullet (bool is_Shooting)
+    {
+      if (is_Shooting == true){
+       
+            GameObject CloneBullet = Instantiate( Bullet,Bullet_position.transform.position, Quaternion.identity) as GameObject;
+         
+        }
     }
 }
